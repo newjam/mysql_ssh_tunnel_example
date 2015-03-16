d0741c6dbd813ed3fe023e3f66409031b7061f7d
James Newman <jameshildrethnewman@gmail.com>
2015-03-16 16:36:33 -0700

    refactored Tunnel to be a re-usable and configurable component.

---
+++ src/main/java/com/github/newjam/test/tunnel/Main.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright Pixatel
+ */
+
+package com.github.newjam.test.tunnel;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author James Newman <james@pixatel.com>
+ */
+public class Main {
+  
+  private final static Logger log = LoggerFactory.getLogger(Main.class);
+  
+  public static void main(String...args) throws IOException, ClassNotFoundException, SQLException {
+    Properties props = new Properties();
+    props.load(Main.class.getClassLoader().getResourceAsStream("application.properties"));
+    ExecutorService service = Executors.newSingleThreadExecutor();
+    
+    try(Tunnel tunnel = new Tunnel(props)) {
+    
+      service.execute(tunnel);
+
+      tunnel.waitForConnection();
+
+      Class.forName("com.mysql.jdbc.Driver");
+
+      final String DATABASE_URL = props.getProperty("DATABASE_URL");
+      final String DATABASE_USERNAME = props.getProperty("DATABASE_USERNAME");
+      final String DATABASE_PASSWORD = props.getProperty("DATABASE_PASSWORD");
+
+      Connection conn = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+
+      Statement stmt = conn.createStatement();
+
+      final String APPROVED_CLIP_COUNT_QUERY = "SELECT COUNT(*) AS approved_clip_count FROM clip WHERE status = 'A'";
+
+      ResultSet result = stmt.executeQuery(APPROVED_CLIP_COUNT_QUERY);
+      result.beforeFirst();
+      result.next();
+
+      long approvedClipCount = result.getLong("approved_clip_count");
+
+      log.debug("There are {} approved clips.", approvedClipCount);
+    
+    } finally {
+      service.shutdownNow();
+    }
+    
+  }
+}
--- src/main/java/com/github/newjam/test/tunnel/Tunnel.java
+++ src/main/java/com/github/newjam/test/tunnel/Tunnel.java
@@ -1,14 +1,10 @@
 package com.github.newjam.test.tunnel;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.security.PublicKey;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 import net.schmizz.sshj.SSHClient;
@@ -16,106 +12,116 @@ import net.schmizz.sshj.connection.channel.direct.LocalPortForwarder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Tunnel {
-  
-  private final static CountDownLatch tunnelEstablishedLatch = new CountDownLatch(1);
+public class Tunnel implements Runnable, Closeable {
   
   private final static Logger log = LoggerFactory.getLogger(Tunnel.class);
   
-  private final static Properties applicationProperties = new Properties();
+  private final CountDownLatch tunnelEstablishedLatch = new CountDownLatch(1);
   
+  private final String SSH_USERNAME;
+  private final String SSH_PASSWORD;
+  private final String REMOTE_HOST;
+  private final int REMOTE_PORT;
+  private final int LOCAL_PORT;
   
-  private static String SSH_USERNAME;
-  private static String SSH_PASSWORD;
-  private static int REMOTE_PORT;
-  private static int LOCAL_PORT;
-  private static String DATABASE_USERNAME;
-  private static String DATABASE_PASSWORD;
-  private static String REMOTE_HOST;
-  private static String DATABASE_URL;
-  
-  
-  static {
-    try {
-      applicationProperties.load(Tunnel.class.getResourceAsStream("application.properties"));
-      
-      SSH_USERNAME = applicationProperties.getProperty("SSH_USERNAME");
-      SSH_PASSWORD = applicationProperties.getProperty("SSH_PASSWORD");
-      REMOTE_PORT = Integer.parseInt(applicationProperties.getProperty("REMOTE_PORT"));
-      LOCAL_PORT = Integer.parseInt(applicationProperties.getProperty("LOCAL_PORT"));
-      DATABASE_USERNAME = applicationProperties.getProperty("DATABASE_USERNAME");
-      DATABASE_PASSWORD = applicationProperties.getProperty("DATABASE_PASSWORD");
-      REMOTE_HOST = applicationProperties.getProperty("REMOTE_HOST");
-      DATABASE_URL = applicationProperties.getProperty("DATABASE_URL");
-    } catch (IOException ex) {
-      log.error("", ex);
-    }
+  private SSHClient ssh = null;
+  private ServerSocket ss = null;
+
+  public Tunnel(Properties props) {
+    this(
+        props.getProperty("REMOTE_HOST")
+      , props.getProperty("SSH_USERNAME")
+      , props.getProperty("SSH_PASSWORD")
+      , Integer.parseInt(props.getProperty("REMOTE_PORT"))
+      , Integer.parseInt(props.getProperty("LOCAL_PORT"))
+    );
   }
   
-  public static boolean verify(String string, int i, PublicKey pk) {
+  private Tunnel(
+      String remotehost
+    , String username
+    , String password
+    , int remotePort
+    , int localPort
+  ) {
+    this.SSH_USERNAME = username;
+    this.SSH_PASSWORD = password;
+    this.REMOTE_HOST = remotehost;
+    this.REMOTE_PORT = remotePort;
+    this.LOCAL_PORT = localPort;
+  }
+  
+  private static boolean hostKeyVerify(String string, int i, PublicKey pk) {
     return true;
   }
-  
-  public static void tunnel() {
+
+  public void waitForConnection() {
     try {
-      SSHClient ssh = new SSHClient();
-      try {
-        ssh.addHostKeyVerifier(Tunnel::verify);
-        ssh.connect(REMOTE_HOST);
-        ssh.authPassword(SSH_USERNAME, SSH_PASSWORD);
-        
-        log.debug("SSH connection {} authenticated", ssh.isAuthenticated() ? "is" : "is not");
-        
-        final LocalPortForwarder.Parameters params
-          = new LocalPortForwarder.Parameters("127.0.0.1", LOCAL_PORT, "127.0.0.1", REMOTE_PORT);
-
-        final ServerSocket ss = new ServerSocket();
-        ss.setReuseAddress(true);
-        ss.bind(new InetSocketAddress(params.getLocalHost(), params.getLocalPort()));
-
-        try {
-          LocalPortForwarder forwarder = ssh.newLocalPortForwarder(params, ss);
-          tunnelEstablishedLatch.countDown();
-          forwarder.listen();
-        } finally {
-          log.debug("Stopping port forwarder.");
-          ss.close();
-        }
-      } finally {
-        log.debug("Disconnecting ssh.");
-        ssh.disconnect();
-        ssh.close();
-      }
-    }catch(IOException ex) {
-      log.error("Error tunneling", ex);
+      tunnelEstablishedLatch.await();
+      Thread.sleep(50);
+    } catch (InterruptedException ex) {
+      log.debug("Error waiting for connection to be established.", ex);
     }
   }
+ 
+  @Override
+  public void run() {
+    try {
+      log.debug("Opening {}", this);
+      
+      ssh = new SSHClient();
+      ss = new ServerSocket();
+
+      ssh.addHostKeyVerifier(Tunnel::hostKeyVerify);
+      ssh.connect(REMOTE_HOST);
+      ssh.authPassword(SSH_USERNAME, SSH_PASSWORD);
+
+      log.debug("SSH connection {} authenticated", ssh.isAuthenticated() ? "is" : "is not");
+
+      final LocalPortForwarder.Parameters params
+        = new LocalPortForwarder.Parameters("127.0.0.1", LOCAL_PORT, "127.0.0.1", REMOTE_PORT);
+
+      ss.setReuseAddress(true);
+      ss.bind(new InetSocketAddress(params.getLocalHost(), params.getLocalPort()));
+
+      LocalPortForwarder forwarder = ssh.newLocalPortForwarder(params, ss);
+      
+      tunnelEstablishedLatch.countDown();
+      forwarder.listen();
+    } catch (IOException ex) {
+      log.error("Error establishing {}.", this, ex);
+    } finally {
+      log.debug("Terminated {}", this);
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "Tunnel from localhost:" + LOCAL_PORT + " to " + SSH_USERNAME + "@" + REMOTE_HOST + ":" + REMOTE_PORT;
+  }
   
-  public static void main(String...args) throws ClassNotFoundException, SQLException, InterruptedException {
+  @Override
+  public void close() throws IOException {
+    if(ssh != null || ss != null) {
+      log.debug("Closing {}", this);
+    } else {
+      log.warn("{} is already closed.", this);
+    }
     
-    Thread tunneler = new Thread(Tunnel::tunnel);
-    tunneler.start();
+    if(ssh != null) {
+      log.debug("Closing SSHClient.");
+      if(ssh.isConnected()) {
+        ssh.disconnect();
+      }
+      ssh.close();
+      ssh = null;
+    }
     
-    tunnelEstablishedLatch.await();
-    Thread.sleep(100);
-    
-    Class.forName("com.mysql.jdbc.Driver");
-    
-    Connection conn = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
-    
-    Statement stmt = conn.createStatement();
-    
-    final String APPROVED_CLIP_COUNT_QUERY = "SELECT COUNT(*) AS approved_clip_count FROM clip WHERE status = 'A'";
-    
-    ResultSet result = stmt.executeQuery(APPROVED_CLIP_COUNT_QUERY);
-    result.beforeFirst();
-    result.next();
-    
-    long approvedClipCount = result.getLong("approved_clip_count");
-    
-    log.debug("There are {} approved clips.", approvedClipCount);
-    
-    tunneler.stop();
+    if(ss != null) {
+      log.debug("Closing ServerSocket.");
+      ss.close();
+      ss = null;
+    }
     
   }
   
